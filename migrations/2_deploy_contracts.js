817ef307a77c414208ed822c51495b03d1228dda
susruth <susruth.praker@gmail.com>
2017-12-08 09:47:30 +1100

    Updated Migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,9 @@
-var ERC20 = artifacts.require("./ERC20.sol");
 var RASERC20 = artifacts.require("./RepublicAtomicSwapERC20.sol");
 var RASEther = artifacts.require("./RepublicAtomicSwapEther.sol");
+var RASERC721 = artifacts.require("./RepublicAtomicSwapERC721.sol");
 
 module.exports = function(deployer) {
-//   deployer.deploy(ERC20);
   deployer.deploy(RASEther);
-//   deployer.link(ERC20, RASERC20);
   deployer.deploy(RASERC20);
+  deployer.deploy(RASERC721);
 };

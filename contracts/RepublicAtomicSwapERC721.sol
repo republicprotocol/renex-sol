4aa5a5255f98a3accf5bef9bfff3e1c76065ab98
susruth <susruth.praker@gmail.com>
2018-01-10 08:44:31 +1100

    updated erc721 atomic swapping contract

--- contracts/RepublicAtomicSwapERC721.sol
+++ contracts/RepublicAtomicSwapERC721.sol
@@ -3,52 +3,66 @@ pragma solidity ^0.4.4;
 import "./ERC721.sol";
 
 contract RepublicAtomicSwapERC721 {
-  ERC721 erc721;
-  
-  struct Trade {
-    address contractAddress;
-    address trader;
-    address tradee;
+
+  struct Swap {
+    address to;
+    address from;
     uint tokenID;
     uint time;
+    bytes32 secretKey;
+    address contractAddress;
   }
-
   mapping (bytes32 => bool) public status;
-  mapping (bytes32 => Trade) locker;
+  mapping (bytes32 => Swap) lockbox;
+  ERC721 erc721;
 
-  function depositERC721(address to, address contractAddress, uint tokenID, bytes32 secretLock) public payable {
+  function deposit(address to, bytes32 secretLock, address contractAddress, uint tokenID) public {
     require(!status[secretLock]);
+
     erc721 = ERC721(contractAddress);
     erc721.transferFrom(msg.sender, address(this), tokenID);
-    locker[secretLock].contractAddress = contractAddress;
-    locker[secretLock].tokenID = tokenID;
-    locker[secretLock].tradee = to;
-    locker[secretLock].trader = msg.sender;
-    locker[secretLock].time = now;  
+
+    lockbox[secretLock].to = to;
+    lockbox[secretLock].from = msg.sender;
+    lockbox[secretLock].tokenID = tokenID;
+    lockbox[secretLock].time = now;
+    lockbox[secretLock].secretKey = 0x0;
+    lockbox[secretLock].contractAddress = contractAddress;
     status[secretLock] = true;
   } 
 
-  function check(bytes32 secretLock) public constant returns (uint, address) {
-    require(locker[secretLock].tradee == msg.sender || locker[secretLock].trader == msg.sender);
-    return (locker[secretLock].tokenID, locker[secretLock].contractAddress);
+  function checkTokenID(bytes32 secretLock) public constant returns (uint, address) {
+    require(lockbox[secretLock].to == msg.sender || lockbox[secretLock].from == msg.sender);
+    return (lockbox[secretLock].tokenID, lockbox[secretLock].contractAddress);
   }
 
-  function withdrawErc721(bytes secretKey) public {
-    bytes32 secretLock = keccak256(secretKey);
-    Trade memory t = locker[secretLock];
+  function checkSecretKey(bytes32 secretLock) public constant returns (bytes32) {
+    require(lockbox[secretLock].to == msg.sender || lockbox[secretLock].from == msg.sender);
+    return lockbox[secretLock].secretKey;
+  }
+
+  function withdraw(bytes32 secretKey) public {
+    bytes32 secretLock = sha256(secretKey);
     require(status[secretLock]);
-    require(t.tradee == msg.sender);
-    erc721 = ERC721(t.contractAddress);
-    erc721.transfer(msg.sender, t.tokenID);
+
+    Swap memory swap = lockbox[secretLock];
+    erc721 = ERC721(swap.contractAddress);
+    erc721.transfer(swap.to, swap.tokenID);
+
+    lockbox[secretLock].tokenID = 0;
+    lockbox[secretLock].secretKey = secretKey;
     status[secretLock] = false;
   }
 
-  function revertEther(bytes32 secretLock) public {
-    Trade memory t = locker[secretLock];
-    require(now - t.time >= 1 days);
+  function expire(bytes32 secretLock) public {
     require(status[secretLock]);
-    erc721 = ERC721(t.contractAddress);
-    erc721.transfer(msg.sender, t.tokenID);
+    require(now - lockbox[secretLock].time >= 1 days);
+
+    Swap memory swap = lockbox[secretLock];
+    erc721 = ERC721(swap.contractAddress);
+    erc721.transfer(swap.from, swap.tokenID);
+
+    lockbox[secretLock].tokenID = 0;
     status[secretLock] = false;
   }
 

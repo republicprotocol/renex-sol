a95f77ef87dcc72282332480ad3e0dbc45cebad0
susruth <susruth.praker@gmail.com>
2018-01-10 08:45:47 +1100

    Modified deploy contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,14 @@
 var RASERC20 = artifacts.require("./RepublicAtomicSwapERC20.sol");
 var RASEther = artifacts.require("./RepublicAtomicSwapEther.sol");
 var RASERC721 = artifacts.require("./RepublicAtomicSwapERC721.sol");
-
+var TestERC20 = artifacts.require("./TestERC20.sol");
+var den = artifacts.require("./den.sol");
+var TestERC721 = artifacts.require("./TestERC721.sol");
 module.exports = function(deployer) {
   deployer.deploy(RASEther);
   deployer.deploy(RASERC20);
+  deployer.deploy(den);
   deployer.deploy(RASERC721);
+  deployer.deploy(TestERC20);
+  deployer.deploy(TestERC721);
 };

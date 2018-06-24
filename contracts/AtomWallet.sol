31f7ef41cb1db474cddd838109c6b19b4d2deba6
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-06-24 11:47:57 +1000

    Updated AtomWallet

--- contracts/AtomWallet.sol
+++ contracts/AtomWallet.sol
@@ -2,58 +2,35 @@ pragma solidity 0.4.24;
 
 contract AtomWallet {
 
-    struct Order {
-        uint8 parity;
-        uint8 orderType;
-        uint64 expiry;
-        uint64 tokens;        
-        uint256 priceC; uint256 priceQ;
-        uint256 volumeC; uint256 volumeQ;
-        uint256 minimumVolumeC; uint256 minimumVolumeQ;
-        uint256 nonceHash;
-        address trader;
+    struct Match {
+        bytes32 buyID;
+        bytes32 sellID;
+        uint32 buyToken;
+        uint32 sellToken;
+        uint256 lowTokenValue;
+        uint256 highTokenValue;
     }
 
-    mapping (bytes32=>Order) public orders;
-    mapping (bytes32=>bytes32) public matches;
-    mapping (address=>uint256) public bonds;
+    mapping (bytes32=>Match) getSettlementDetails;
 
-    function() public payable {
-        bonds[msg.sender] += msg.value;
-    }
-
-    function submitOrder(
-        bytes32 _id,
-        uint8 _orderType,
-        uint8 _parity,
-        uint64 _expiry,
-        uint64 _tokens,
-        uint16 _priceC, uint16 _priceQ,
-        uint16 _volumeC, uint16 _volumeQ,
-        uint16 _minimumVolumeC, uint16 _minimumVolumeQ,
-        uint256 _nonceHash
-    ) public {
-        orders[_id] = Order({
-            orderType: _orderType,
-            parity: _parity,
-            expiry: _expiry,
-            tokens: _tokens,
-            priceC: _priceC, priceQ: _priceQ,
-            volumeC: _volumeC, volumeQ: _volumeQ,
-            minimumVolumeC: _minimumVolumeC, minimumVolumeQ: _minimumVolumeQ,
-            nonceHash: _nonceHash,
-            trader: 0x0 
+    function setSettlementDetails(bytes32 _buyID, bytes32 _sellID, uint32 _buyToken, uint32 _sellToken, uint256 _lowTokenValue, uint32 _highTokenValue) public {
+        getSettlementDetails[_buyID] = Match({
+            buyID: _buyID,
+            sellID: _sellID,
+            buyToken: _buyToken,
+            sellToken: _sellToken,
+            lowTokenValue: _lowTokenValue,
+            highTokenValue: _highTokenValue
         });
-    }
 
-    function submitMatch(bytes32 _buy, bytes32 _sell) public {
-        matches[_buy] = _sell;
-        matches[_sell] = _buy;
-    }
-
-    function withdraw(uint256 _amount) public {
-        require(_amount <= bonds[msg.sender]);
-        bonds[msg.sender] -= _amount;
+        getSettlementDetails[_sellID] = Match({
+            buyID: _buyID,
+            sellID: _sellID,
+            buyToken: _buyToken,
+            sellToken: _sellToken,
+            lowTokenValue: _lowTokenValue,
+            highTokenValue: _highTokenValue
+        });
     }
 
 }

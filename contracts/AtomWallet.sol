275fa4621f1b9fe5e19abbeccdd48a46f023e14a
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-06-24 12:07:57 +1000

    Updated set settlement details function

--- contracts/AtomWallet.sol
+++ contracts/AtomWallet.sol
@@ -7,29 +7,40 @@ contract AtomWallet {
         bytes32 sellID;
         uint32 buyToken;
         uint32 sellToken;
-        uint256 lowTokenValue;
-        uint256 highTokenValue;
+        uint256 buyValue;
+        uint256 sellValue;
     }
 
     mapping (bytes32=>Match) public getSettlementDetails;
 
     function setSettlementDetails(bytes32 _buyID, bytes32 _sellID, uint32 _buyToken, uint32 _sellToken, uint256 _lowTokenValue, uint32 _highTokenValue) public {
+        uint256 buyValue;
+        uint256 sellValue;
+
+        if (_buyToken < _sellToken) {
+            buyValue = _highTokenValue;
+            sellValue = _lowTokenValue;
+        } else {
+            sellValue = _highTokenValue;
+            buyValue = _lowTokenValue;
+        }
+        
         getSettlementDetails[_buyID] = Match({
             buyID: _buyID,
             sellID: _sellID,
             buyToken: _buyToken,
             sellToken: _sellToken,
-            lowTokenValue: _lowTokenValue,
-            highTokenValue: _highTokenValue
+            buyValue: buyValue,
+            sellValue: sellValue
         });
 
         getSettlementDetails[_sellID] = Match({
-            buyID: _buyID,
-            sellID: _sellID,
-            buyToken: _buyToken,
-            sellToken: _sellToken,
-            lowTokenValue: _lowTokenValue,
-            highTokenValue: _highTokenValue
+            buyID: _sellID,
+            sellID: _buyID,
+            buyToken: _sellToken,
+            sellToken: _buyToken,
+            buyValue: sellValue,
+            sellValue: buyValue
         });
     }
 

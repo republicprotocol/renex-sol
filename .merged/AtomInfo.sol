4415e53ee13f678be26b4811af0c2c28085e4946
negaex <gh@negaex.com>
2018-07-10 17:13:33 +1000

    Updated configuration

--- .merged/AtomInfo.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.24;
-
-contract AtomInfo {
-    mapping (bytes32 => bytes) public getOwnerAddress;
-    
-    function setOwnerAddress(bytes32 _orderID, bytes _owner) public {
-       getOwnerAddress[_orderID] = _owner;
-    }
-}
--- .merged/AtomNetwork.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.24;
-
-contract AtomNetwork {
-    mapping (bytes32 => bytes) public swapDetails;
-
-    function submitDetails(bytes32 _orderID, bytes _swapDetails) public {
-        swapDetails[_orderID] = _swapDetails;
-    }
-}

3205f0ffe1a4d089ec8888bc01a2611b92f8a200
negaex <gh@negaex.com>
2018-07-19 12:15:26 +1000

    .travis.yml: Install typescript; updated tab sizes

--- test-ts/AtomicInfo.ts
+++ test-ts/AtomicInfo.ts
@@ -8,24 +8,24 @@ const Info = artifacts.require("AtomicInfo");
 
 context("AtomicInfo", function () {
 
-  let info, id, swap, addr;
+    let info, id, swap, addr;
 
-  before(async function () {
-    info = await Info.new();
-  });
+    before(async function () {
+        info = await Info.new();
+    });
 
-  it("can submit and retreive swap details", async () => {
-    id = "0x1234";
-    swap = "0x567890";
-    await info.submitDetails(id, swap);
-    assert.equal((await info.swapDetails(id)), swap);
-  });
+    it("can submit and retreive swap details", async () => {
+        id = "0x1234";
+        swap = "0x567890";
+        await info.submitDetails(id, swap);
+        assert.equal((await info.swapDetails(id)), swap);
+    });
 
-  it("can submit and retreive addresses", async () => {
-    id = "0x1234";
-    addr = "0x567890";
-    await info.setOwnerAddress(id, addr);
-    assert.equal((await info.getOwnerAddress(id)), addr);
-  });
+    it("can submit and retreive addresses", async () => {
+        id = "0x1234";
+        addr = "0x567890";
+        await info.setOwnerAddress(id, addr);
+        assert.equal((await info.getOwnerAddress(id)), addr);
+    });
 
 });
--- test-ts/AtomicSwap.ts
+++ test-ts/AtomicSwap.ts
@@ -9,38 +9,38 @@ import * as SHA256 from "crypto-js/sha256";
 
 contract("AtomicSwap", function (accounts) {
 
-  let swap, swapRefund, secretLock;
-  const swapID = `0x${SHA256(Math.random().toString()).toString()}`;
-  const alice = accounts[0];
-  const bob = accounts[1];
-  const secret = `0x${SHA256('Secret').toString()}`;
+    let swap, swapRefund, secretLock;
+    const swapID = `0x${SHA256(Math.random().toString()).toString()}`;
+    const alice = accounts[0];
+    const bob = accounts[1];
+    const secret = `0x${SHA256('Secret').toString()}`;
 
-  before(async function () {
-    swap = await Swap.new();
-    swapRefund = await Swap.new();
-    secretLock = secretLock = `0x${SHA256(SHA256('Secret')).toString()}`;
-  });
+    before(async function () {
+        swap = await Swap.new();
+        swapRefund = await Swap.new();
+        secretLock = secretLock = `0x${SHA256(SHA256('Secret')).toString()}`;
+    });
 
-  it("can initiate an atomic swap", async () => {
-    var ts = Math.round((new Date()).getTime() / 1000) + 10000;
-    await swap.initiate(swapID, bob, secretLock, ts, { from: alice, value: 100000 }).should.not.be.rejected;
-  });
+    it("can initiate an atomic swap", async () => {
+        var ts = Math.round((new Date()).getTime() / 1000) + 10000;
+        await swap.initiate(swapID, bob, secretLock, ts, { from: alice, value: 100000 }).should.not.be.rejected;
+    });
 
-  it("can audit an atomic swap", async () => {
-    await swap.audit(swapID).should.not.be.rejected;
-  });
+    it("can audit an atomic swap", async () => {
+        await swap.audit(swapID).should.not.be.rejected;
+    });
 
-  it("can redeem an atomic swap", async () => {
-    await swap.redeem(swapID, secret, { from: bob }).should.not.be.rejected;
-  });
+    it("can redeem an atomic swap", async () => {
+        await swap.redeem(swapID, secret, { from: bob }).should.not.be.rejected;
+    });
 
-  it("can audit secret of an atomic swap", async () => {
-    await swap.auditSecret(swapID).should.not.be.rejected;
-  });
+    it("can audit secret of an atomic swap", async () => {
+        await swap.auditSecret(swapID).should.not.be.rejected;
+    });
 
-  it("can refund an atomic swap", async () => {
-    await swapRefund.initiate(swapID, bob, secretLock, 0, { from: alice, value: 100000 }).should.not.be.rejected;
-    await swapRefund.refund(swapID, { from: alice }).should.not.be.rejected;
-  });
+    it("can refund an atomic swap", async () => {
+        await swapRefund.initiate(swapID, bob, secretLock, 0, { from: alice, value: 100000 }).should.not.be.rejected;
+        await swapRefund.refund(swapID, { from: alice }).should.not.be.rejected;
+    });
 
 });

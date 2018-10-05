e883ed74f55742793a0e0f09f17dcb611c1cd20f
Negaex <gh@negaex.com>
2018-10-06 08:02:37 +1000

    .solcover.js: Ignore test contracts

--- .solcover.js
+++ .solcover.js
@@ -1,8 +1,22 @@
 module.exports = {
     skipFiles: [
-        'test/tokens/DGXToken.sol',
-        'test/tokens/OMGToken.sol',
-        'test/tokens/TUSDToken.sol',
-        'test/tokens/ZRXToken.sol',
+        // Contract for building bindings
+        "Bindings.sol",
+
+        // Migration contract
+        "migrations/Migrations.sol",
+
+        // Contracts for assisting the tests
+        "test/ApprovingBroker.sol",
+        "test/DisapprovingToken.sol",
+        "test/Time.sol",
+        "test/VersionedContract.sol",
+        "test/PreciseToken.sol",
+
+        // Source code for tokens supported on RenEx
+        "test/tokens/DGXToken.sol",
+        "test/tokens/OMGToken.sol",
+        "test/tokens/TUSDToken.sol",
+        "test/tokens/ZRXToken.sol",
     ],
 };
